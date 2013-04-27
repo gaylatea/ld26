0441a7c906c4ea0a9c9dd8b94f0e8a0ea530febe
Patrick Adair <padair@he.net>
2013-04-26 20:08:51 -0700

    Initial game files.

---
+++ conf.lua
@@ -0,0 +1,10 @@
+-- New config changes for this Love2d game.
+function love.conf(t)
+    t.title             = "LD26"
+    t.author            = "Blue Heaven"
+    t.url               = "https://github.com/blueheaven/ld26"
+    t.screen.width      = 1280
+    t.screen.height     = 720
+    t.screen.fsaa       = 2
+    t.modules.joystick  = false
+end
---
+++ main.lua
@@ -0,0 +1,5 @@
+-- ld26 main.lua
+-- Callbacks for Love2d.
+function love.draw()
+   -- Main draw loop.
+end
