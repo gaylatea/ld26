5e152df0a12b103e14522632a145fe35153353c6
Patrick Adair <padair@he.net>
2013-04-28 15:57:59 -0700

    Add in clickable buttons for the splash screen to do Normal/Potato mode.

--- states/splashScreen.lua
+++ states/splashScreen.lua
@@ -1,4 +1,6 @@
 -- Splash screen mode.
+require("lib/button")
+
 splashScreen    = {}
 splashScreen_mt = { __index = splashScreen }
 
@@ -10,9 +12,15 @@ function splashScreen:new()
 
   local background = love.graphics.newImage("assets/splash.png")
 
+  local buttons   = {
+    normal = Button:new(900, 450, 200, 32, "New Game", self.normalMode),
+    potato = Button:new(900, 520, 200, 32, "Potato Mode", self.potatoMode),
+  }
+
   return setmetatable({
     sounds      = sounds,
     background  = background,
+    buttons     = buttons,
   }, splashScreen_mt)
 end
 
@@ -22,20 +30,33 @@ end
 
 function splashScreen:click(x, y, button)
   -- Process mouse clicks on this screen.
-  love.audio.play(self.sounds.start)
+  if button ~= "l" then return end
 
-  if button == "r" then
-    player = Player:new(true)
-  else
-    player = Player:new()
+  for i, button in pairs(self.buttons) do
+    if button:is_inside(x, y) then
+      button:click()
+      love.audio.play(self.sounds.start)
+
+      game          = gameScreen:new()
+      currentScreen = game
+    end
   end
+end
 
-  game          = gameScreen:new()
-  currentScreen = game
+function splashScreen.potatoMode()
+  -- Callback for potato mode button.
+  player = Player:new(true)
+end
+
+function splashScreen.normalMode()
+  -- Callback for normal button.
+  player = Player:new()
 end
 
 function splashScreen:draw()
   -- Draw this screen for the current frame.
   love.graphics.draw(self.background, 0, 0)
-  love.graphics.print("Click the mouse to start", 750, 500)
+  for i, button in pairs(self.buttons) do
+    button:draw()
+  end
 end
